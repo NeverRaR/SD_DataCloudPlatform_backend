c7958bcf5d1d28a9ba66e72d0f9aa2b01fa30cd8
Never <897013703@qq.com>
2021-02-04 22:27:56 +0800

    修改cookie

--- src/main/java/com/neverrar/datacloudplatform/backend/controller/UserController.java
+++ src/main/java/com/neverrar/datacloudplatform/backend/controller/UserController.java
@@ -3,14 +3,18 @@ package com.neverrar.datacloudplatform.backend.controller;
 import com.neverrar.datacloudplatform.backend.error.*;
 import com.neverrar.datacloudplatform.backend.model.User;
 import com.neverrar.datacloudplatform.backend.repository.UserRepository;
+import com.neverrar.datacloudplatform.backend.service.AuthenticationService;
 import com.neverrar.datacloudplatform.backend.util.HashHelper;
 import com.neverrar.datacloudplatform.backend.util.Result;
 import com.neverrar.datacloudplatform.backend.view.UserInformation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
@@ -25,7 +29,7 @@ public class UserController {
     private UserRepository userRepository;
 
     @Autowired
-    private StringRedisTemplate template;
+    private AuthenticationService authenticationService;
 
     @PostMapping // Map ONLY POST Requests
     public @ResponseBody
@@ -48,37 +52,44 @@ public class UserController {
 
     @GetMapping
     public @ResponseBody
-    Result<UserInformation> getUser (@RequestParam String sessionId) {
-        // @ResponseBody means the returned String is the response, not a view name
-        // @RequestParam means it is a parameter from the GET or POST request
-        String id=template.opsForValue().get(sessionId);
-        if(id==null) return Result.wrapErrorResult(new InvalidSessionIdError());
-        Optional<User>  optionalUser=userRepository.findById(id);
-        if(!optionalUser.isPresent()) return Result.wrapErrorResult(new UserNotExistedError());
-        return Result.wrapSuccessfulResult(new UserInformation(optionalUser.get()));
+    Result<UserInformation> getUser (@CookieValue(value = "sessionId",
+            defaultValue = "noSession") String sessionId) {
+        User user=authenticationService.getUser(sessionId);
+        if(user==null) {
+            return Result.wrapErrorResult(new InvalidSessionIdError());
+        }
+        return Result.wrapSuccessfulResult(new UserInformation(user));
     }
 
     @PostMapping(path="/session")
-    public @ResponseBody Result<String> getLoginToken(@RequestBody User user,
+    public @ResponseBody Result<UserInformation> getLoginToken(@RequestBody User body,
                                                       HttpServletResponse response, HttpServletRequest request) {
 
-        String sessionId;
-        Optional<User> opt=userRepository.findById(user.getId());
-        if(!opt.isPresent()) return Result.wrapErrorResult(new UserNotExistedError());
-        String passwordHashed=HashHelper.computeSha256Hash(user.getPassword()+opt.get().getSalt());
-        if(!opt.get().getPassword().equals(passwordHashed)) return Result.wrapErrorResult(new WrongPasswordError());
-        double seed=ThreadLocalRandom.current().nextDouble();
-        sessionId=HashHelper.computeMD5Hash(user.getId()+ seed);
-        if(sessionId==null) return Result.wrapErrorResult(new NullError());
-        template.opsForValue().set(sessionId,user.getId(),3, TimeUnit.HOURS);
-        return Result.wrapSuccessfulResult(sessionId);
+        String sessionId=authenticationService.createSessionId(body.getId(),body.getPassword());
+        if(sessionId==null){
+            response.setStatus(401);
+            return null;
+        }
+        ResponseCookie responseCookie = ResponseCookie.from("sessionId", sessionId)
+                .maxAge(3* 24* 60 * 60)
+                .httpOnly(true)
+                .path("/")
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, responseCookie.toString());
+
+        User user=authenticationService.getUser(sessionId);
+        return Result.wrapSuccessfulResult(new UserInformation(user));
     }
 
     @DeleteMapping(path="/session")
-    public @ResponseBody Result<String> invalidateSessionId(@RequestParam String sessionId) {
-        String id=template.opsForValue().get(sessionId);
-        if(id==null) return Result.wrapErrorResult(new InvalidSessionIdError());
-        template.delete(sessionId);
+    public @ResponseBody
+    Result<String> logout(@CookieValue(value = "sessionId",
+            defaultValue = "noSession") String sessionId,HttpServletResponse response){
+        authenticationService.invalidateSessionId(sessionId);
+        Cookie cookie=new Cookie("sessionId",null);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
         return Result.wrapSuccessfulResult("logout!");
     }
 }
