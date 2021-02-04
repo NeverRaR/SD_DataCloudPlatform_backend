6c6368e27b627c898cf80292c1ff61d2d95a03f1
Never <897013703@qq.com>
2021-02-04 22:12:05 +0800

    OSS配置

--- src/main/java/com/neverrar/datacloudplatform/backend/config/CorsConfig.java
+++ src/main/java/com/neverrar/datacloudplatform/backend/config/CorsConfig.java
@@ -1,7 +1,6 @@
 package com.neverrar.datacloudplatform.backend.config;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
